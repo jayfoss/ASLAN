e1cb9e0d992f8706087157350ba5843f2393c83b
Jerzy Foss <44465055+jayfoss@users.noreply.github.com>
2024-12-07 22:31:23 +0000

    Instruction: Finish tests

--- typescript/test/instruction.spec.ts
+++ typescript/test/instruction.spec.ts
@@ -1,16 +1,36 @@
-import { ASLANParser } from '../src/aslan-parser';
+import { ASLANEndDataInstruction, ASLANInstruction, ASLANParser } from '../src/aslan-parser';
+import { deepCopy } from '../src/utils';
 
 describe('ASLANParser Instruction', () => {
   let parser: ASLANParser;
+  let contentEvents: ASLANInstruction[];
+  let endEvents: ASLANInstruction[];
+  let endDataEvents: ASLANEndDataInstruction[];
 
   beforeEach(() => {
     parser = new ASLANParser();
+    contentEvents = [];
+    parser.addEventListener('content', (event) => {
+      contentEvents.push(deepCopy(event) as ASLANInstruction);
+    });
+    endEvents = [];
+    parser.addEventListener('end', (event) => {
+      endEvents.push(deepCopy(event) as ASLANInstruction);
+    });
+    endDataEvents = [];
+    parser.addEventListener('end_data', (event) => {
+      endDataEvents.push(deepCopy(event) as ASLANEndDataInstruction);
+    });
   });
 
   test('parses simple string with instructions', () => {
     const result = parser.parse(
       '[asland_styled_text][aslanp][aslani_bold][aslani_color:red]This is bold and red text.[aslanp][aslani_italic][aslani_underline]This is italic and underlined text.[aslanp][aslani_size:large][aslani_font:monospace]This is large monospace text.',
     );
+
+    expect(contentEvents).toMatchSnapshot('instruction-events-content');
+    expect(endEvents).toMatchSnapshot('instruction-events-end');
+    expect(endDataEvents).toMatchSnapshot('instruction-events-end-data');
     expect(result).toEqual({
       "_default": null,
       "styled_text": [
@@ -25,6 +45,9 @@ describe('ASLANParser Instruction', () => {
     const result = parser.parse(
       '[asland_styled_text:l][aslanp][aslani_bold][aslani_color:red]This is bold and red text.[asland_styled_text][aslanp][aslani_italic][aslani_color:blue]This is italic and blue text.',
     );
+    expect(contentEvents).toMatchSnapshot('instruction-events-content');
+    expect(endEvents).toMatchSnapshot('instruction-events-end');
+    expect(endDataEvents).toMatchSnapshot('instruction-events-end-data');
     expect(result).toEqual({
       "_default": null,
       "styled_text": [
@@ -37,6 +60,9 @@ describe('ASLANParser Instruction', () => {
     const result = parser.parse(
       '[asland_styled_text:f][aslanp][aslani_bold][aslani_color:red]This is bold and red text.[asland_styled_text][aslanp][aslani_italic][aslani_color:blue]This is italic and blue text.',
     );
+    expect(contentEvents).toMatchSnapshot('instruction-events-content');
+    expect(endEvents).toMatchSnapshot('instruction-events-end');
+    expect(endDataEvents).toMatchSnapshot('instruction-events-end-data');
     expect(result).toEqual({
       "_default": null,
       "styled_text": [
@@ -46,16 +72,22 @@ describe('ASLANParser Instruction', () => {
   });
 
   test('parses simple string with instructions no part', () => {
-    parser.addEventListener('end', (event) => {
-      console.log(event);
-    });
     const result = parser.parse(
       '[asland_test][aslana][asland][aslano][asland_styled_text][aslani_bold][aslani_color:red]This is bold and red text.[asland_next]Next item',
     );
+    expect(contentEvents).toMatchSnapshot('instruction-events-content');
+    expect(endEvents).toMatchSnapshot('instruction-events-end');
+    expect(endDataEvents).toMatchSnapshot('instruction-events-end-data');
     expect(result).toEqual({
       "_default": null,
-      "styled_text": "This is bold and red text.",
+      test: [
+        {
+          next: 'Next item',
+          styled_text: 'This is bold and red text.'
+        }
+      ]
     });
   });
+
 
 });
